bbfde0e89c9a60730a027dd57184ebd986dd7ae4
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2022-05-10 17:11:09 +0200

    Task-54013: Add dlp webapps module

--- dlp-webapp/webpack.watch.js
+++ dlp-webapp/webpack.watch.js
@@ -1,11 +1,20 @@
 const path = require('path');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 
 const webpackProductionConfig = require('./webpack.prod.js');
 
+// the display name of the war
+const app = 'dlp';
+
+// add the server path to your server location path
+
+const exoServerPath = "/exo-server";
+
+
 module.exports = merge(webpackProductionConfig, {
   output: {
-    path: '/exo-server/webapps/dlp/',
-    filename: 'js/[name].bundle.js'
-  }
-});
+    path: path.resolve(`${exoServerPath}/webapps/${app}/`)
+  },
+  mode: 'development',
+  devtool: 'inline-source-map'
+})
