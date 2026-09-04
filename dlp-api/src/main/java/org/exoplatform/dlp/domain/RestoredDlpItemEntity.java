ea0da127b8043cb33d324fcaaf3bc0455f961cf7
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2026-09-04 14:42:16 +0100

    feat: Upgrade to Spring Boot 4.1 - Meeds-io/meeds#3889 (#95)

--- dlp-api/src/main/java/org/exoplatform/dlp/domain/RestoredDlpItemEntity.java
+++ dlp-api/src/main/java/org/exoplatform/dlp/domain/RestoredDlpItemEntity.java
@@ -1,23 +1,22 @@
 package org.exoplatform.dlp.domain;
 
-import org.exoplatform.commons.api.persistence.ExoEntity;
-
-import jakarta.persistence.*;
 import java.util.Calendar;
 
-/**
- * Entity for restored Dlp item.
- */
+import io.meeds.common.persistence.PortableSequence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+
 @Entity(name = "RestoredDlpItemEntity")
-@ExoEntity
 @Table(name = "RESTORED_DLP_ITEMS")
 @NamedQuery(name = "RestoredDlpItemEntity.findRestoredDlpItemByReference", query = "SELECT q FROM RestoredDlpItemEntity q WHERE q.reference = :itemReference")
-
 public class RestoredDlpItemEntity {
 
   @Id
-  @SequenceGenerator(name = "SEQ_RESTORED_DLP_ITEMS_ID", sequenceName = "SEQ_RESTORED_DLP_ITEMS_ID", allocationSize = 1)
-  @GeneratedValue(strategy = GenerationType.AUTO, generator = "SEQ_RESTORED_DLP_ITEMS_ID")
+  @PortableSequence(name = "SEQ_RESTORED_DLP_ITEMS_ID")
   @Column(name = "RESTORED_ITEM_ID")
   private Long     id;
 
